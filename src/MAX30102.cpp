9a2d339194e536767a63591adea677d415cf50d2
Supachock Taenjetriw <ampandj@gmail.com>
2023-11-08 15:47:37 +0700

    readRegister 8 function

--- src/MAX30102.cpp
+++ src/MAX30102.cpp
@@ -2,7 +2,6 @@
 
 MAX30102::MAX30102()
 {
-
 }
 
 bool MAX30102::begin(uint8_t I2C_ADDRESS, uint8_t SENSOR_PARTID, uint32_t I2C_SPEED)
@@ -13,4 +12,19 @@ bool MAX30102::begin(uint8_t I2C_ADDRESS, uint8_t SENSOR_PARTID, uint32_t I2C_SP
 
     Wire.setClock(I2C_SPEED);
     return true;
+}
+
+uint8_t MAX30102::readRegister8(uint8_t address, uint8_t reg)
+{
+    Wire.beginTransmission(address);
+    Wire.write(reg); // register to read
+    Wire.endTransmission();
+
+    Wire.requestFrom(address, (uint8_t)1);
+    if (Wire.available())
+    {
+        uint8_t value = Wire.read();
+        return value;
+    }else
+        return 0;
 }
--- src/MAX30102.h
+++ src/MAX30102.h
@@ -10,6 +10,9 @@ class MAX30102
 public:
     MAX30102(void);
     bool begin(uint8_t I2C_ADDRESS = MAX30102_I2C_ADDRESS, uint8_t SENSOR_PARTID = MAX30102_PARTID, uint32_t I2C_SPEED = I2C_SPEED_STANDARD);
+
+    // Low-level I2C communication
+    uint8_t readRegister8(uint8_t address, uint8_t reg);
 };
 
 #endif
