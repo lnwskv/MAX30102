c3d28b9dacef944bc84703d4d37ebe73a2977e22
Supachock Taenjetriw <ampandj@gmail.com>
2023-11-09 00:55:51 +0700

    ..

--- src/MAX30102.cpp
+++ src/MAX30102.cpp
@@ -191,7 +191,7 @@ uint16_t MAX30102::fillFIFO(void)
 
 void MAX30102::bitMask(uint8_t reg, uint8_t mask, uint8_t thing)
 {
-    uint8_t byte = readRegister8(Wire, reg);
+    uint8_t byte = readRegister8(MAX30102_I2C_ADDRESS, reg);
 
     byte = byte & mask;
 
