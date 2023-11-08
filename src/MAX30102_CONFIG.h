97cc1fa11c204587b7d134ac03c35e7b30da769e
Supachock Taenjetriw <ampandj@gmail.com>
2023-11-08 16:46:56 +0700

    update 3

--- src/MAX30102_CONFIG.h
+++ src/MAX30102_CONFIG.h
@@ -1,9 +1,9 @@
 #ifndef MAX30102_CONFIG_H
 #define MAX30102_CONFIG_H
 
-#define I2C_ADDRESS 0x57     // 7-bit I2C Address, Note that MAX30102 has the same I2C address and Part ID
-#define PARTID_REGISTER 0xFF // PARTID register address of MAX30102
-#define MAX30102_EXPECTEDPARTID 0x15;
+#define MAX30102_I2C_ADDRESS 0x57     // 7-bit I2C Address, Note that MAX30102 has the same I2C address and Part ID
+#define MAX30102_PARTID_REGISTER 0xFF // PARTID register address of MAX30102
+#define MAX30102_EXPECTEDPARTID 0x15
 
 #define I2C_SPEED_STANDARD 100000
 #define I2C_SPEED_FAST 400000
