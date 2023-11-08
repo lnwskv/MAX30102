0a5a835331e08da172a818c606ecad8bcd9c4c70
Supachock Taenjetriw <ampandj@gmail.com>
2023-11-08 16:49:57 +0700

    update 4

--- src/MAX30102.h
+++ src/MAX30102.h
@@ -9,7 +9,7 @@ class MAX30102
 {
 public:
     MAX30102(void);
-    bool begin(uint8_t I2C_ADDRESS = MAX30102_I2C_ADDRESS, uint8_t I2C_REGISTER = MAX30102_PARTID_REGISTER, uint32_t I2C_SPEED = I2C_SPEED_STANDARD);
+    bool begin(uint8_t MAX30102_ADDRESS = MAX30102_I2C_ADDRESS, uint8_t MAX30102_REGISTER = MAX30102_PARTID_REGISTER, uint32_t I2C_SPEED = I2C_SPEED_STANDARD);
 
     // Low-level I2C communication
     uint8_t readRegister8(uint8_t address, uint8_t reg);
